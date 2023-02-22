4c73f05fbb266bf09583acbf0064e0cc56883806
Talha Jamal <talhajamal11@gmail.com>
2023-02-23 00:52:30 +0500

    Adding a few functions to Production Code

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import numpy as np
 import datetime as dt
 import requests
 
+# Configurations of Libraries
+pd.set_option('display.max_columns', 500)
 
+'''
 def get_pricing(ticker, start, end):
     ticker_pricing = yf.download(ticker, start=start, end=end)
     return ticker_pricing
@@ -46,10 +49,40 @@ df['Market Cap $bn'] = df['Market Cap $bn'].str.strip("£$bn")
 df['Market Cap $bn'] = pd.to_numeric(df['Market Cap $bn'])
 df = df.sort_values('Market Cap $bn',ascending=False)
 return df
+'''
+
+
+def pricing_df(ticker, start, stop):
+    # This function returns a dataframe for the pricing and volume data for a single ticker
+    beg = dt.datetime.strptime(start, '%Y/%m/%d')
+    end = dt.datetime.strptime(stop, '%Y/%m/%d')
+    price_df = yf.download(ticker, beg, end)
+    price_df.dropna(inplace=True)
+    return price_df
+
+
+def daily_returns(df):
+    # Create a dataframe for the return statistics of a provided dataframe
+    returns = pd.DataFrame()
+    returns["Daily Returns"] = df["Adj Close"].pct_change()
+    returns["Daily Log Returns"] = np.log(df["Adj Close"]/df["Adj Close"].shift(1))
+    returns["Cumulative Returns"] = ((returns["Daily Returns"]) + 1).cumprod()
+    returns["Annualized Volatility"] = returns["Daily Log Returns"].rolling(252).std() * np.sqrt(252)
+    return returns
+
+
+def moving_averages(df):
+    # Calculate 21D and 200D Moving Averages
+    m_avg = pd.DataFrame()
+    m_avg["21D SMA"] = df["Adj Close"].rolling(21).mean()
+    m_avg["200D SMA"] = df["Adj Close"].rolling(252).mean()
+    return m_avg
 
 
 if __name__ == '__main__':
-    end = dt.datetime.now().date()
-    start = '2010/01/01'
-    print(start)
-    # AMZN = get_pricing('AMZN', '2020-01-01', '2022-01-01')
+    AMZN = pricing_df('AMZN', '2022/01/01', '2023/01/22')
+    AMZN_returns = daily_returns(AMZN)
+    AMZN_m_avg = moving_averages(AMZN)
+    print(AMZN.head())
+    print(AMZN_returns)
+    print(AMZN_m_avg)
